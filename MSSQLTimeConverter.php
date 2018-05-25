690eca55e0eee2fdd940ff067929ef82b8a266d9
Kyrylo Lytvynov <themaiby0@gmail.com>
2018-05-25 13:04:15 +0300

    init

---
+++ MSSQLTimeConverter.php
@@ -0,0 +1,41 @@
+<?php
+
+class MSSQLTimeConverter
+{
+    /**
+     * @param float $time
+     * @return DateTime
+     */
+    public function fromFloat(float $time) {
+        $explodedMSTime = explode('.', $time);
+
+        $days = $explodedMSTime[0];
+        $seconds = '0.' . $explodedMSTime[1];
+
+        $startDate = new DateTime('1900-01-01');
+
+        try {
+            // int val is always days count since 1900-01-01 00:00:00
+            $daysInterval = DateInterval::createFromDateString($days . ' days');
+
+            // percent value of day time
+            $humanSeconds = intval( 86400 * $seconds);
+
+            $secInterval = DateInterval::createFromDateString($humanSeconds. ' seconds');
+
+            $resultDate = $startDate->add($daysInterval)->add($secInterval);
+        } catch (Exception $e) {
+            echo $e;
+        }
+
+        return $resultDate;
+    }
+}
+
+
+// ### Example
+$a = new MSSQLTimeConverter();
+$MSDateTime = 43243.5382623071;
+
+$humanDate = $a->fromFloat($MSDateTime);
+echo $humanDate->format('Y-m-d H:i:s.u');
